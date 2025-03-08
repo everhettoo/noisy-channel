148be50721c10ac3cb05db6e7c6e341c76dd7c0d
everhettoo <everhettoo@gmail.com>
2025-03-08 16:38:37 +0800

    Fixed unit tests for both 1word and 2 words

--- tests/test_proba_dist_2word.py
+++ tests/test_proba_dist_2word.py
@@ -6,56 +6,69 @@ import utility as util
 class Test(TestCase):
     def setUp(self):
         # 588,124,220,187 (vocab-size)
-        self.denominator = 588124220187
+        self.denominator = 225955251755.0
 
         # Defining the probability P(w) for a word in corpus based on its occurrence.
-        self.p = ProbaDistributor(util.datafile('../data/count_1w.tsv'))
+        self.p = ProbaDistributor(util.datafile('../data/count_2w.tsv'))
         print(f'Total words in corpus\t:{self.p.denominator:,}.\r\n')
 
-        self.assertEqual(self.p.denominator, self.p.denominator)
+        self.assertEqual(self.denominator, self.p.denominator)
 
-    def test_pw_orange(self):
-        # count(orange) = 37,316,112
-        w = 'orange'
+    def test_double_word1(self):
+        # count(3rd floor) = 272,543
+        w = '3rd floor'
 
         w_cnt = self.p[w]
         print(f'Total count({w})\t\t:{w_cnt:,}.')
 
-        # P(orange) =  37,316,122 / 588,124,220,187.0 = 6.34494 x 10^-05
+        # P(3rd floor) =  	272,543 / 225,955,251,755 = 1.20618 x 10^-06
         proba = self.p(w)
         print(f'P({w})\t\t\t\t:{proba:,}.')
         self.assertEqual(w_cnt / self.p.denominator, proba)
 
-    def test_pw_apple(self):
-        # count(apple) = 50,551,171
-        w = 'apple'
+    def test_double_word2(self):
+        # count(2nd year) = 275,672
+        w = '2nd year'
 
         w_cnt = self.p[w]
         print(f'Total count({w})\t\t:{w_cnt:,}.')
 
-        # P(apple) =  50,551,171 / 588,124,220,187.0 = 8.59532 x 10^-05
+        # P(2nd year) =  275,672 / 225,955,251,755 = 1.22002 x 10^-06
         proba = self.p(w)
         print(f'P({w})\t\t\t\t:{proba:,}.')
         self.assertEqual(w_cnt / self.p.denominator, proba)
 
     def test_relative_proba(self):
-        # Defining the probability P(w) for a word in corpus based on its occurrence.
-        pw = ProbaDistributor(util.datafile('../data/count_1w.tsv'))
+        # count(3rd floor) = 272,543
+        a = '3rd floor'
+        # count(2nd year) = 275,672
+        b = '2nd year'
 
-        # count(orange) = 37,316,112
-        a = 'apple'
-        # count(apple) = 50,551,171
-        b = 'orange'
-        condition = pw(a) > pw(b)
-        print(f'{pw(a)} > {pw(b)}: {condition}.')
+        condition = self.p(a) < self.p(b)
+        print(f'{self.p(a)} < {self.p(b)}: {condition}.')
         self.assertTrue(condition)
 
     def test_pw_unknown1(self):
-        w = 'dooohhh'
+        w = 'dooohhh good'
 
-        # P(apple) =  1 / 588,124,220,187.0 = 1.70032 x 10^-12.
+        # P(dooohhh good) =  1 / 225,955,251,755 = 4.42565 x 10^-12
         proba = self.p(w)
         print(f'P({w})\t\t\t\t:{proba:,}.')
         self.assertEqual(1 / self.p.denominator, proba)
 
-        #
+    def test_pw_unknown2(self):
+        w = 'korenge'
+
+        # P(korenge) =  1 / 225,955,251,755 = 4.42565 x 10^-12
+        proba = self.p(w)
+        print(f'P({w})\t\t\t\t:{proba:,}.')
+        self.assertEqual(1 / self.p.denominator, proba)
+
+    def test_relative_proba_for_unknown(self):
+        a = 'dooohhh good'
+        b = 'korenge'
+
+        # 4.42565 x 10^-12 = 4.42565 x 10^-12
+        condition = self.p(a) == self.p(b)
+        print(f'{self.p(a)} = {self.p(b)}: {condition}.')
+        self.assertTrue(condition)
